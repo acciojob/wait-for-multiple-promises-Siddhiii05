ffeda9ba6bfe5aca06aa573d95052f7e85b5cd3c
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-08-03 20:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 const output = document.getElementById("output");
 
 function createPromise() {
-	const delay = Math.random * 2 + 1
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(delay);
-		},delay * 1000);
-	})
+    const delay = Math.random() * 2 + 1;
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(delay);
+        }, delay * 1000);
+    });
 }
 
 const promise1 = createPromise();
 const promise2 = createPromise();
 const promise3 = createPromise();
 
-const startTime = performance.now();
-
 Promise.all([promise1, promise2, promise3]).then((results) => {
-	const endTime = performance.now();
-	const totalTime = (endTime - startTime)/1000;
 
-	output.innerHTML = "";
-	results.forEach((time, index) => {
-		const row = document.createElement("tr");
-		row.innerHTML = `
-			<td>Promise ${index + 1}</td>
-			<td>${time.toFixed(3)}</td>
-		`;
-	})
-	output.appendChild(row);
+    output.innerHTML = "";
 
-	const totalRow = document.createElement("tr");
-	totalRow.innerHTML = `
-		<td>Total</td>
-		<td>${totalTime.toFixed(3)}</td>
-	`;
-	output.appendChild(totalRow);
-})
+    results.forEach((time, index) => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>Promise ${index + 1}</td>
+            <td>${time.toFixed(3)}</td>
+        `;
+
+        output.appendChild(row);
+    });
+
+    // Maximum promise time
+    const totalTime = Math.max(...results);
+
+    const totalRow = document.createElement("tr");
+
+    totalRow.innerHTML = `
+        <td>Total</td>
+        <td>${totalTime.toFixed(3)}</td>
+    `;
+
+    output.appendChild(totalRow);
+});
